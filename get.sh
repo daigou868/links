17e39a3edfd3ba55c108629db4c525c237d1aa77
daigou868 <hello@868daigou.com>
2016-09-27 16:14:49 +0930

    Update get.sh

--- get.sh
+++ get.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 youtube-dl https://www.youtube.com/watch?v=Qrgqrv0AYx8
 youtube-dl https://www.youtube.com/watch?v=HCPnZ51dO6A
-
 youtube-dl https://www.youtube.com/watch?v=boBpC8E7JMo
-youtube-dl https://www.youtube.com/watch?v=0gID-z2-gVo
-youtube-dl https://www.youtube.com/watch?v=vDUDlygZOTQ
-youtube-dl https://www.youtube.com/watch?v=1UyJG2b8FMc
-youtube-dl https://www.youtube.com/watch?v=dfIuSSA_27w
-youtube-dl https://www.youtube.com/watch?v=EvgM8rDsvRI
-
 youtube-dl https://www.youtube.com/watch?v=Nfxd3FVYpi8
 youtube-dl https://www.youtube.com/watch?v=KQQGzccFskM
 youtube-dl https://www.youtube.com/watch?v=2uoqOvIlj0w
