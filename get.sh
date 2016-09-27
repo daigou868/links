dce60877bed976e08bd75ac885b110d642e03292
daigou868 <hello@868daigou.com>
2016-09-27 13:13:02 +0930

    Create get.sh

---
+++ get.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+brew update
+brew upgrade
+youtube-dl https://www.youtube.com/watch?v=JGulAZnnTKA
+youtube-dl https://www.youtube.com/watch?v=mdJamKoEVjA
+youtube-dl https://www.youtube.com/watch?v=Qrgqrv0AYx8
+youtube-dl https://www.youtube.com/watch?v=zD3XHUnrjyY
+youtube-dl https://www.youtube.com/watch?v=FF72jugiHQc
+youtube-dl https://www.youtube.com/watch?v=HCPnZ51dO6A
+youtube-dl https://www.youtube.com/watch?v=boBpC8E7JMo
+youtube-dl https://www.youtube.com/watch?v=0gID-z2-gVo
+youtube-dl https://www.youtube.com/watch?v=vDUDlygZOTQ
+youtube-dl https://www.youtube.com/watch?v=1UyJG2b8FMc
+youtube-dl https://www.youtube.com/watch?v=dfIuSSA_27w
+youtube-dl https://www.youtube.com/watch?v=EvgM8rDsvRI
